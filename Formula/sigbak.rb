4168076fc59f08b749d4f30a27ac6db1fdede439
Tim van der Molen <tim@kariliq.nl>
2021-12-09 11:47:16 +0100

    Don't depend on make on Linux

--- Formula/sigbak.rb
+++ Formula/sigbak.rb
@@ -2,11 +2,15 @@ class Sigbak < Formula
   homepage "https://github.com/tbvdm/sigbak"
   head "https://github.com/tbvdm/sigbak.git", branch: "portable"
 
-  depends_on "make" => :build
   depends_on "pkg-config" => :build
   depends_on "libressl"
   depends_on "protobuf-c"
   depends_on "sqlite"
+
+  # Xcode provides GNU make 3.81, which is too old
+  on_macos do
+    depends_on "make" => :build
+  end
 
   def install
     ENV.prepend_path "PATH", Formula["make"].opt_libexec/"gnubin" if OS.mac?
--- Formula/sigtop.rb
+++ Formula/sigtop.rb
@@ -2,9 +2,13 @@ class Sigtop < Formula
   homepage "https://github.com/tbvdm/sigtop"
   head "https://github.com/tbvdm/sigtop.git", branch: "portable"
 
-  depends_on "make" => :build
   depends_on "pkg-config" => :build
   depends_on "libressl"
+
+  # Xcode provides GNU make 3.81, which is too old
+  on_macos do
+    depends_on "make" => :build
+  end
 
   def install
     ENV.prepend_path "PATH", Formula["make"].opt_libexec/"gnubin" if OS.mac?
